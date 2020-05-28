710473014e884f3e40c722df2c77e7387da581f3
zhangbingna <13710541469@163.com>
2020-05-28 16:08:05 +0800

    five

--- src/main/java/com/testerhome/hogwarts/wework/contact/Department.java
+++ src/main/java/com/testerhome/hogwarts/wework/contact/Department.java
@@ -8,9 +8,10 @@ import static io.restassured.RestAssured.given;
 
 public class Department {
     public Response list(String id){
-        return given().param("access_token", Wework.getToken())
+        return given().log().all()
+                .param("access_token", Wework.getToken())
                 .param("id",id)
         .when().get("https://qyapi.weixin.qq.com/cgi-bin/department/list")
-        .then().statusCode(200).extract().response();
+        .then().log().all().statusCode(200).extract().response();
     }
 }
--- src/test/java/com/testerhome/hogwarts/wework/contact/DepartmentTest.java
+++ src/test/java/com/testerhome/hogwarts/wework/contact/DepartmentTest.java
@@ -2,6 +2,7 @@ package com.testerhome.hogwarts.wework.contact;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.Matchers.equalTo;
 
 
 class DepartmentTest {
@@ -9,6 +10,8 @@ class DepartmentTest {
     @Test
     void list() {
         Department department=new Department();
-        department.list("").then().statusCode(200);
+        department.list("").then().statusCode(200).body("department.name[2]",equalTo("测试部"));
+        department.list("3").then().statusCode(200).body("department.name[0]",equalTo("测试部"));
+
     }
 }
