3f6fc8a2b18e7a0dc8cc14d1cf6e52c7226ddddb
zhangbingna <13710541469@163.com>
2020-07-07 20:00:55 +0800

    xueqiu

---
+++ src/test/java/Xueqiu.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Xueqiu {
+    @Test
+    public void  testLogin(){
+        given()
+                .header("")
+    }
+}
